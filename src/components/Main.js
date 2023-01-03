ea27b7a3935b955da5f9560de493df76b5508619
Joli Holmes <holmesjoli@gmail.com>
2023-01-03 18:42:05 -0500

    added additional components to switch between

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,10 +7,15 @@ import * as Classify from "../pages/predict/Classify";
 import * as Train from "../pages/predict/Train";
 import * as Optimize from "../pages/predict/Optimize";
 import * as Calibration from "../pages/fairness/Calibration";
+import * as FalsePositive from "../pages/fairness/FalsePositive";
+import * as FalseNegative from "../pages/fairness/FalseNegative";
+import * as Compas from "../pages/case_studies/COMPAS";
+import * as PublicPolicy from "../pages/case_studies/PublicPolicy";
+import * as DecisionAid from "../pages/deliberation/DecisionAid";
+import * as RiskFramework from "../pages/deliberation/RiskFramework";
+import * as StakeholderMapping from "../pages/deliberation/Stakeholders";
 
 class Content extends React.Component{
-
-    // area!;
 
     constructor(config) {
         super(config);
@@ -33,6 +38,27 @@ class Content extends React.Component{
                 break;
             case 'calibration' :
                 component = <Calibration.Visualization/>;
+                break;
+            case 'falsePositive' :
+                component = <FalsePositive.Visualization/>;
+                break;
+            case 'falseNegative' :
+                component = <FalseNegative.Visualization/>;
+                break;
+            case 'compas' :
+                component = <Compas.Visualization/>;
+                break;
+            case 'publicPolicy' :
+                component = <PublicPolicy.Visualization/>;
+                break;
+            case 'decisionAid' :
+                component = <DecisionAid.Visualization/>;
+                break;
+            case 'riskFramework' :
+                component = <RiskFramework.Visualization/>;
+                break;
+            case 'stakeholderMapping' :
+                component = <StakeholderMapping.Visualization/>;
                 break;
         }
         return component;
