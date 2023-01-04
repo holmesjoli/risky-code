89802d0078f094f7c5a599d783ae90a1ce17491d
Joli Holmes <holmesjoli@gmail.com>
2023-01-04 02:13:17 -0500

    added initial model components

--- src/components/Model.js
+++ src/components/Model.js
@@ -1,8 +1,20 @@
 export default function Model() {
     return(
         <div className="Model">
-            <div className="Variables"></div>
-            <div className="Selected-Variables"></div>
+            <div className="Variables">
+               <h5>Data Variables</h5>
+               <div className="Variable-Container">
+                    <span className="Variable">Item type</span>
+                    <span className="Variable">Print</span>
+                    <span className="Variable">Soiled</span>
+                    <span className="Variable">Pastel</span>
+                    <span className="Variable">Item color</span>
+                    <span className="Variable">Care type</span>
+               </div>
+            </div>
+            <div className="Selected-Variables">
+                <h5>Model Variables</h5>
+            </div>
         </div>
     )
 }
