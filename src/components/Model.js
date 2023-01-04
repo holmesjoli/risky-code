d53b2cdceb442bfba89545de16ddc891af6ca2e6
Joli Holmes <holmesjoli@gmail.com>
2023-01-04 02:24:01 -0500

    initial attempt at drag and drop

--- src/components/Model.js
+++ src/components/Model.js
@@ -1,8 +1,31 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
+// https://rootstack.com/en/blog/how-do-i-use-drag-and-drop-react
 export default function Model() {
     
+    const dragItem = useRef();
+    const dragOverItem = useRef();
     const [list, setList] = useState(['Item type','Print','Soiled','Pastel','Item color','Care type']);
+
+    const dragStart = (e, position) => {
+        dragItem.current = position;
+        console.log(e.target.innerHTML);
+    };
+
+    const dragEnter = (e, position) => {
+        dragOverItem.current = position;
+        console.log(e.target.innerHTML);
+    };
+
+    const drop = (e) => {
+        const copyListItems = [...list];
+        const dragItemContent = copyListItems[dragItem.current];
+        copyListItems.splice(dragItem.current, 1);
+        copyListItems.splice(dragOverItem.current, 0, dragItemContent);
+        dragItem.current = null;
+        dragOverItem.current = null;
+        setList(copyListItems);
+    };
 
     return(
 
