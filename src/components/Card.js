f8106237667be9f8425685aa9d23de0276de1e71
Joli Holmes <holmesjoli@gmail.com>
2023-01-05 17:44:47 -0500

    updates

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,27 +1,77 @@
 export default function Card() {
-    return(
 
+    const data = [
+        {
+          id: '0'
+        },
+        {
+          id: '1'
+        },
+        {
+          id: '2'
+        },
+        {
+          id: '3'
+        },
+        {
+          id: '4'
+        },
+        {
+          id: '5'
+        },
+        {
+          id: '6'
+        },
+        {
+          id: '7'
+        },
+        {
+          id: '8'
+        },
+        {
+          id: '9'
+        },
+        {
+          id: '10'
+        },
+        {
+          id: '11'
+        },
+        {
+          id: '12'
+        },
+        {
+          id: '13'
+        },
+        {
+          id: '14'
+        },
+        {
+          id: '15'
+        },
+        {
+          id: '16'
+        },
+        {
+          id: '17'
+        },
+        {
+          id: '18'
+        },
+        {
+          id: '19'
+        }
+      ]
+
+    return(
         <div className="Card-Container">
-            <div className="Card"></div>
-            <div className="Card"></div>
-            <div className="Card"></div>
-            <div className="Card"></div>
-            <div className="Card"></div>
-            <div className="Card"></div>
-            <div className="Card"></div>
-            <div className="Card"></div>
-            <div className="Card"></div>
-            <div className="Card"></div>
-            <div className="Card"></div>
-            <div className="Card"></div>
-            <div className="Card"></div>
-            <div className="Card"></div>
-            <div className="Card"></div>
-            <div className="Card"></div>
-            <div className="Card"></div>
-            <div className="Card"></div>
-            <div className="Card"></div>
-            <div className="Card"></div>
+
+            {data.map(({id, name}, index) => {
+                return( 
+                <div key={id} className="Card"></div>
+                )
+            })
+            }
         </div>
     )
 }
--- src/components/Model.js
+++ src/components/Model.js
@@ -1,62 +1,82 @@
-import { useState, useRef } from 'react';
+import { useState } from 'react';
 // import arrow from "./utils/img/arrow.svg";
-import { DragDropContext } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
+const modelVariables = [
+    {
+      id: 'type',
+      name: 'Item type'
+    },
+    {
+      id: 'print',
+      name: 'Print'
+    },
+    {
+      id: 'soiled',
+      name: 'Soiled'
+    },
+    {
+      id: 'pastel',
+      name: 'Pastel'
+    },
+    {
+      id: 'color',
+      name: 'Item color'
+    },
+    {
+      id: 'care',
+      name: 'Care type'
+    }
+  ]
 
 // dragging action adapted from https://rootstack.com/en/blog/how-do-i-use-drag-and-drop-react
+// https://www.freecodecamp.org/news/how-to-add-drag-and-drop-in-react-with-react-beautiful-dnd/
 export default function Model() {
+
+    const [variables, updateVariables] = useState(modelVariables);
+
+    function handleOnDragEnd(result) {
+        if (!result.destination) return;
     
-    const dragItem = useRef();
-    const dragOverItem = useRef();
-    const [list, setList] = useState(['Item type','Print','Soiled','Pastel','Item color','Care type']);
-
-    const dragStart = (e, position) => {
-        dragItem.current = position;
-        console.log(e.target.innerHTML);
-    };
-
-    const dragEnter = (e, position) => {
-        dragOverItem.current = position;
-        console.log(e.target.innerHTML);
-    };
-
-    const drop = (e) => {
-        const copyListItems = [...list];
-        const dragItemContent = copyListItems[dragItem.current];
-        copyListItems.splice(dragItem.current, 1);
-        copyListItems.splice(dragOverItem.current, 0, dragItemContent);
-        dragItem.current = null;
-        dragOverItem.current = null;
-        setList(copyListItems);
-    };
+        const items = Array.from(variables);
+        const [reorderedItem] = items.splice(result.source.index, 1);
+        items.splice(result.destination.index, 0, reorderedItem);
+    
+        updateVariables(items);
+      }
 
     return(
-
         <div className="Model">
-            <div className="Variables">
-               <h5>Data Variables</h5>
-               <div className="Variable-Container">
-               <>
-                    {
-                    list&&
-                    list.map((item, index) => (
-                    <span 
-                        className="Variable" 
-                        key={index} 
-                        onDragStart={(e) => dragStart(e, index)}
-                        onDragEnter={(e) => dragEnter(e, index)}
-                        onDragEnd={drop}
-                        draggable> 
-                        {item}
-                    </span>
-                    ))}
-                    </>
-               </div>
-            </div>
-            {/* <div></div> */}
-            {/* <img src={arrow} alt="Your SVG" /> */}
-            <div className="Selected-Variables">
-                <h5>Model Variables</h5>
-            </div>
+            <DragDropContext onDragEnd={handleOnDragEnd}>
+                <Droppable droppableId="droppable-variable">
+                    {(provided) => (
+                    <div className="droppable-variable" {...provided.droppableProps} ref={provided.innerRef}>
+                        <div className="Data-Variables">
+                            <h5>Data Variables</h5>
+                            <div>
+                                {variables.map(({id, name}, index) => {
+
+                                return (
+                                    <Draggable key={id} draggableId={id} index={index}>
+                                        {(provided) => (
+                                            <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} className="Variable">
+                                                { name }
+                                            </div>
+                                        )}
+                                    </Draggable>
+                                    );
+                                })}
+                                {provided.placeholder}
+                            </div>
+                        </div>
+                        <div className="droppable-variable" {...provided.droppableProps} ref={provided.innerRef}>
+                            <h5>Model Variables</h5>
+                        </div> 
+                        {/* {provided.placeholder} */}
+                    </div>
+                )}
+                </Droppable>
+            </DragDropContext>
         </div>
     )
 }
