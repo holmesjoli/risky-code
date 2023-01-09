cc7c867ec810ab4c59c0440ba1f36754c4382d39
Joli Holmes <holmesjoli@gmail.com>
2023-01-08 19:43:08 -0500

    removed unused files

--- src/components/constants.js
+++
@@ -1,4 +0,0 @@
-export const COLUMN_NAMES = {
-    DATA_VARIABLES: 'Data Variables',
-    MODEL_VARIABLES: 'Model Variables'
-}
--- src/components/tasks.js
+++
@@ -1,9 +0,0 @@
-import { COLUMN_NAMES } from "./constants";
-
-const {DATA_VARIABLES} = COLUMN_NAMES;
-export const tasks = [
-    {id: 1, name: 'Item 1', column: DATA_VARIABLES},
-    {id: 2, name: 'Item 2', column: DATA_VARIABLES},
-    {id: 3, name: 'Item 3', column: DATA_VARIABLES},
-    {id: 4, name: 'Item 4', column: DATA_VARIABLES },
-];
