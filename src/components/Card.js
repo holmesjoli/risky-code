9ceea2b4472c3582587bc33b655ef0e938a5fa4f
Joli Holmes <holmesjoli@gmail.com>
2023-01-09 14:39:14 -0500

    moved cards into the global file

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,72 +1,11 @@
-export default function Card() {
+import { CARDS } from "../utils/global";
 
-    const data = [
-        {
-          id: '0'
-        },
-        {
-          id: '1'
-        },
-        {
-          id: '2'
-        },
-        {
-          id: '3'
-        },
-        {
-          id: '4'
-        },
-        {
-          id: '5'
-        },
-        {
-          id: '6'
-        },
-        {
-          id: '7'
-        },
-        {
-          id: '8'
-        },
-        {
-          id: '9'
-        },
-        {
-          id: '10'
-        },
-        {
-          id: '11'
-        },
-        {
-          id: '12'
-        },
-        {
-          id: '13'
-        },
-        {
-          id: '14'
-        },
-        {
-          id: '15'
-        },
-        {
-          id: '16'
-        },
-        {
-          id: '17'
-        },
-        {
-          id: '18'
-        },
-        {
-          id: '19'
-        }
-      ]
+export default function Card() {
 
     return(
         <div className="Card-Container">
 
-            {data.map(({id, name}, index) => {
+            {CARDS.map(({id, name}, index) => {
                 return( 
                 <div key={id} className="Card"></div>
                 )
