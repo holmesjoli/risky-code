81fd24f093e430f8e2158f48729933282ec0c752
Joli Holmes <holmesjoli@gmail.com>
2023-01-14 16:07:47 -0500

    added images to train

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,18 @@
+import { importImages } from "./Helper";
 
 export default function Card({items}) {
 
+    const images = importImages()
     console.log(items)
+
     return(
         <div className="Cards-Container Container">
             <h5 className="Small-Margin">Items</h5>
             <div className="Card-Container">
                 {items.map((item) => {
                     return( 
-                        <div key={item.id+"id"}className="Card">
+                        <div key={item.id+"id"}className="Card Flat">
+                            <img src={images[Object.keys(images)[item.id]]} alt="An item of clothing" width="100" height="50" ></img>
                         </div>
                         )
                     })
