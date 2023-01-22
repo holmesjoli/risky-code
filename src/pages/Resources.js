97674c225fb0b3a3d52eb2ceaeabb57e90e37a1a
Joli Holmes <holmesjoli@gmail.com>
2023-01-22 11:25:14 -0500

    added additional resources

--- src/pages/Resources.js
+++ src/pages/Resources.js
@@ -1,6 +1,61 @@
 import Navigation from '../components/Navigation';
 import Header from '../components/Header';
-import { literature }  from "../utils/global";
+
+const literature = {
+
+    "AINOW-2018": {
+        "inline-citation": "(AINOW 2018)",
+        "reference": 'AINOW. 2018. "Algorithmic Accountability Policy Toolkit."'
+    },
+    "Barocas-Selbst-2016": {
+        "inline-citation": "(Barocas and Selbst 2016)",
+        "reference": "Barocas, Solon, and Andrew D Selbst. 2016. &quot;Big Data's Disparate Impact.&quot; California Law Review 104 (3): 671-732."
+    },
+    "Bender-Friedman-2018": {
+        "inline-citation": "(Bender and Friedman 2018)",
+        "reference": "Bender, Emily M., and Batya Friedman. 2018. &quot;Data Statements for Natural Language Processing: Toward Mitigating System Bias and Enabling Better Science.&quot; Transactions of the Association for Computational Linguistics 6: 587-604."
+    },
+    "Benjamin-2019": {
+        "inline-citation": "(Benjamin 2019)",
+        "reference": "Benjamin, Ruha. 2019. Race After Technology: Abolitionist Tools for the New Jim Crow. Medford, MA: Polity Press."
+    },
+    "Friedman-Nissenbaum-1996": {
+        "inline-citation": "(Friedman and Nissenbaum 1996)",
+        "reference": 'Friedman, Batya, and Helen Nissenbaum. 1996. "Bias in Computer Systems." ACM Transactions on Information Systems 14 (3): 330-47.'
+    },
+    "Narayanan-2018": {
+        "inline-citation": "(Narayanan, 2018)",
+        "reference": 'Narayanan, Arvind. 2018. "Tutorial: 21 Fairness Definitions and Their Politics."'
+    },
+    "Selbst-2019": {
+        "inline-citation": "(Selbst et al. 2019)",
+        "reference": "Selbst, Andrew, Danah Boyd, Sorelle Friedler, Suresh Venkatasubramanian, and Janet Vertesi. 2019. &quot;Fairness and Abstraction in Sociotechnical Systems.&quot; In Proceedings of the Conference on Fairness, Accountability, and Transparency, 59-68. FAT '19. ACM."
+    }
+}
+
+const aiExplainability = {
+
+    "AI-blindspot": {
+        "name": "AI Blindspot Workshop",
+        "link": "https://aiblindspot.media.mit.edu/"
+    },
+    "Peoples-Guide-AI": {
+        "name": "A People's Guide to AI",
+        "link": "https://mimionuoha.com/a-peoples-guide-to-ai"
+    },
+    "AI-Equity-Toolkit": {
+        "name": "AI Equity Toolkit",
+        "link": "https://www.aclu-wa.org/AEKit#:~:text=WHAT%20IS%20THE%20AEKit%3F,impacts%2C%20effectiveness%2C%20and%20oversight."
+    },
+    "AI-Fairer-Than-A-Judge": {
+        "name": "Can you make AI fairer than a judge? Play our courtroom algorithm game",
+        "link": "https://www.technologyreview.com/2019/10/17/75285/ai-fairer-than-judge-criminal-risk-assessment-algorithm/amp/"
+    },
+    "Survival-of-Best-Fit": {
+        "name": "Survival of Best Fit",
+        "link": "https://www.survivalofthebestfit.com/game/"
+    }
+}
 
 export function Content() {
     return(
@@ -13,10 +68,20 @@ export function Content() {
                             </p>
                         )
                     })
-                    }
+                }
             </div>
             <div className="Resources-Container Container">
                 <h4>AI Explainability Resources</h4>
+                <ul>
+                    {Object.keys(aiExplainability).map((index) => {
+                        return(
+                            <li key={index} className="AI-Explainability-Reference">
+                                <a href={aiExplainability[index].link} target="_blank">{aiExplainability[index].name}</a>
+                            </li>
+                            )
+                        })
+                    }
+                </ul>
             </div>
         </div>
     )
--- src/utils/global.js
+++ src/utils/global.js
@@ -190,35 +190,3 @@ export const glossary = {
         "link": "Bender-Friedman-2018"
     }
 }
-
-export const literature = {
-
-    "AINOW-2018": {
-        "inline-citation": "(AINOW 2018)",
-        "reference": 'AINOW. 2018. "Algorithmic Accountability Policy Toolkit."'
-    },
-    "Barocas-Selbst-2016": {
-        "inline-citation": "(Barocas and Selbst 2016)",
-        "reference": "Barocas, Solon, and Andrew D Selbst. 2016. &quot;Big Data's Disparate Impact.&quot; California Law Review 104 (3): 671-732."
-    },
-    "Bender-Friedman-2018": {
-        "inline-citation": "(Bender and Friedman 2018)",
-        "reference": "Bender, Emily M., and Batya Friedman. 2018. &quot;Data Statements for Natural Language Processing: Toward Mitigating System Bias and Enabling Better Science.&quot; Transactions of the Association for Computational Linguistics 6: 587-604."
-    },
-    "Benjamin-2019": {
-        "inline-citation": "(Benjamin 2019)",
-        "reference": "Benjamin, Ruha. 2019. Race After Technology: Abolitionist Tools for the New Jim Crow. Medford, MA: Polity Press."
-    },
-    "Friedman-Nissenbaum-1996": {
-        "inline-citation": "(Friedman and Nissenbaum 1996)",
-        "reference": 'Friedman, Batya, and Helen Nissenbaum. 1996. "Bias in Computer Systems." ACM Transactions on Information Systems 14 (3): 330-47.'
-    },
-    "Narayanan-2018": {
-        "inline-citation": "(Narayanan, 2018)",
-        "reference": 'Narayanan, Arvind. 2018. "Tutorial: 21 Fairness Definitions and Their Politics."'
-    },
-    "Selbst-2019": {
-        "inline-citation": "(Selbst et al. 2019)",
-        "reference": "Selbst, Andrew, Danah Boyd, Sorelle Friedler, Suresh Venkatasubramanian, and Janet Vertesi. 2019. &quot;Fairness and Abstraction in Sociotechnical Systems.&quot; In Proceedings of the Conference on Fairness, Accountability, and Transparency, 59-68. FAT '19. ACM."
-    }
-}
