71dc6135a13a0d550a5a2468a833e1ef7935a064
Joli Holmes <holmesjoli@gmail.com>
2023-01-26 10:08:54 -0500

    fixed bug in model

--- src/components/Model.js
+++ src/components/Model.js
@@ -168,7 +168,7 @@ export default function Model({variables, setVariables}) {
       .filter((item) => item.column === columnName)
       .map((item, index) => (
         <MovableItem
-        key={item.id+"Model-Id"}
+          key={index}
           name={item.name}
           currentColumnName={item.column}
           setVariables={setVariables}
