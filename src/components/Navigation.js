f0678f3354c973661c5e26b0e50dc9da038b5407
Joli Holmes <holmesjoli@gmail.com>
2023-02-17 14:33:37 -0500

    added visualization and links to the fairness page

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,40 +4,7 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { navigationData, highlightColor } from "../utils/global";
 import * as d3 from 'd3';
 import { useEffect } from 'react';
-
-function wrap(text, width) {
-    text.each(function () {
-        var text = d3.select(this),
-            words = text.text().split(/\s+/).reverse(),
-            word,
-            line = [],
-            lineNumber = 0,
-            lineHeight = 1.1, // ems
-            x = text.attr("x"),
-            y = text.attr("y"),
-            dy = 0, //parseFloat(text.attr("dy")),
-            tspan = text.text(null)
-                        .append("tspan")
-                        .attr("x", x)
-                        .attr("y", y)
-                        .attr("dy", dy + "em");
-
-        while (word = words.pop()) {
-            line.push(word);
-            tspan.text(line.join(" "));
-            if (tspan.node().getComputedTextLength() > width) {
-                line.pop();
-                tspan.text(line.join(" "));
-                line = [word];
-                tspan = text.append("tspan")
-                            .attr("x", x)
-                            .attr("y", y)
-                            .attr("dy", ++lineNumber * lineHeight + dy + "em")
-                            .text(word);
-            }
-        }
-    });
-}
+import { wrap } from "../utils/global";
 
 const height = 505;
 const width = 260;
--- src/pages/fairness/Calibration.js
+++ src/pages/fairness/Calibration.js
@@ -1,12 +1,62 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, NavLink } from "react-router-dom";
 import { Button } from "@material-ui/core";
+import * as d3 from 'd3';
 import Legend from "../../components/Legend";
 import Navigation from '../../components/Navigation';
 import Description from '../../components/Description';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import Overlay from "../../components/Overlay";
+import data from "../../data/processed/mathematical_fairness.json"
+import { wrap } from "../../utils/global";
+
+function fairnessDefinitions() {
+
+    let n = data.length;
+    let theta = ((Math.PI*2) / n);
+    let width = 500;
+    let height = 500;
+    let radius = 200; 
+
+    for (let i in data) {
+        let angle = (theta * i);
+        data[i].x = (radius * Math.cos(angle)) + width/2;
+        data[i].y = (radius * Math.sin(angle)) + height/2;
+    }
+
+    let svg = d3.select("#Fairness-Chart")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+
+    let nodes = svg.selectAll("circle")
+        .data(data)
+        .enter()
+        .append("a")
+        .attr("href", d=> d.link)
+        .attr("target", "_blank")
+        .append("circle")
+        .attr("cx", d => d.x)
+        .attr("cy", d => d.y)
+        .attr("r", 8)
+        .attr("fill", "white");
+
+    nodes
+        .append("circle")
+        .attr("cx", d => d.x)
+        .attr("cy", d => d.y)
+        .attr("r", 8)
+        .attr("fill", "white");
+
+    svg.append("text")
+        .attr("x", 250)
+        .attr("y", 250)
+        .text("Mathematical fairness definitions")
+        .attr("fill", "white")
+        .attr("font-size", 14)
+        .call(wrap, 40);
+}
 
 function Information() {
     return (
@@ -44,8 +94,13 @@ export default function Classify({config, items, setItems, modules}) {
     };
 
     useEffect(() => {
-        setId(isOpen ? "fairness": "calibration")
+        setId(isOpen ? "fairness": "calibration");
     }, [isOpen])
+
+
+    useEffect(() => {
+        fairnessDefinitions();
+    }, [])
 
     return (
         <div className="App">{
@@ -55,6 +110,7 @@ export default function Classify({config, items, setItems, modules}) {
                 <div className="Container">
                     <h3>introduction to fairness</h3>
                     <p>AI researchers have proposed over 20 mathematical constructions of fairness <NavLink to="/Resources">(Verma and Rubin 2018; Narayanan 2018)</NavLink>. Specifically, this module will review the <span className="Semi-Bold">calibration rate</span>, <span className="Semi-Bold">false positive rate</span>, and <span className="Semi-Bold">false negative rate</span>. In this module, we will learn how to calibrate the model to optimize for these different definitions and see how optimizing for one definition affects the other definitions.</p>
+                    <div className="chart" id="Fairness-Chart"></div>
                 </div>
             </div>
         </Overlay>:
