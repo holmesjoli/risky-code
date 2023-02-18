98f823c75a1b450e0878e6f797952db5a566d892
Joli Holmes <holmesjoli@gmail.com>
2023-02-18 14:42:26 -0500

    updated ui for all pages

--- src/pages/Glossary.js
+++ src/pages/Glossary.js
@@ -136,12 +136,12 @@ export default function Glossary({config, modules}) {
             <div className="Main">
                 <div className="Sidebar-Left">
                     <Description config={config}/>
-                    <div className="Button-Container-Left">
-                        <Button variant="outlined" color="secondary" onClick={() => navigate(-1)}>back</Button>
-                    </div>
                 </div>
                 <Content />
                 <div className="Sidebar-Right">
+                    <div className="Button-Container-Right">
+                        <Button variant="outlined" color="secondary" onClick={() => navigate(-1)}>back</Button>
+                    </div>
                     <Navigation id="glossary" modules={modules}/>
                 </div>
             </div>
--- src/pages/Resources.js
+++ src/pages/Resources.js
@@ -152,12 +152,12 @@ export default function Resources({config, modules}) {
             <div className="Main">
                 <div className="Sidebar-Left">
                     <Description config={config}/>
-                    <div className="Button-Container-Left">
-                        <Button variant="outlined" color="secondary" onClick={() => navigate(-1)}>back</Button>
-                    </div>
                 </div>
                 <Content />
                 <div className="Sidebar-Right">
+                    <div className="Button-Container-Right">
+                        <Button variant="outlined" color="secondary" onClick={() => navigate(-1)}>back</Button>
+                    </div>
                     <Navigation id="resources" modules={modules}/>
                 </div>
             </div>
