58a21946157c47f09ec8631b0e2830eb1f390a83
Joli Holmes <holmesjoli@gmail.com>
2023-02-27 11:28:51 -0500

    updated description

--- src/components/Description.js
+++ src/components/Description.js
@@ -2,8 +2,8 @@
 export default function Card({config}) {
     return(
         <div className="Description">
-            <h2 className="Title">{config.title}</h2>
             <div className="Container-Outlined">
+                <h2 className="Title">{config.title}</h2>
                 <h3 className="Subtitle">{config.subtitle}</h3>
                 <p>{config.descr}</p>
             </div>
