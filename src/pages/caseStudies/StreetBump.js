c58b562c7ead33b1a7da995eabf14715f3b226fd
Joli Holmes <holmesjoli@gmail.com>
2023-03-02 13:24:35 -0500

    updated overlay page

--- src/pages/caseStudies/StreetBump.js
+++ src/pages/caseStudies/StreetBump.js
@@ -9,7 +9,7 @@ import { Consequence, Stakeholders } from "../../components/PolicyScenario";
 import { Terminology, Term } from '../../components/Terminology';
 import { terms } from '../../utils/global';
 import Progress from "../../components/Progress";
-import { BackButton, NextButton } from '../../components/Button';
+import { BackButton, NextButton, NextButtonOverlay } from '../../components/Button';
 import { LeftSideBar, RightSideBar } from "../../components/Sidebar";
 import { Button } from '@material-ui/core';
 
@@ -120,17 +120,19 @@ export default function StreetBump({config, modules}) {
             <Overlay isOpen={isOpen} onClose={toggleOverlay}>
             <div className="Containers-Container">
                 <div className="Container-Fill-Secondary">
-                    <div className="Overlay-Controls">
                         <h3 className="Page-Title">introduction to case studies</h3>
-                        <button
-                            className="Overlay-Close"
-                            type="button"
-                            onClick={toggleOverlay}
-                        />
+                        <div className="Two-Column-Three">
+                            <div>
+                                <div id={chartID} className="chart Margin-Bottom"></div>
+                                <h6>Visualization shows different policy areas where algorithmically informed-decision making is currently in use. Case studies are highlighed in purple. Visualization data created from examples in <NavLink to="/Resources">O'Neil (2016) AINOW (2018), Eubanks (2018), and Obermeyer et al. (2019)</NavLink>.</h6>
+                            </div>
+                            <div>
+                            <h4>algorithmically informed decision-making in use</h4>
+                                <p>Algorithmically informed decision-making tools are now being used in every field. They are used to evaluate prisoners for parole, triage patients in emergency rooms, and predict where and when services might be needed. The next module will look at two different algorithmic case studies and explain how different definitions of mathematical fairness may qapply.</p>
+                                <NextButtonOverlay toggleOverlay={toggleOverlay}/>
+                            </div>
+                        </div>
                     </div>
-                    <p>Algorithmically informed decision-making tools are now being used in every field. They are used to evaluate prisoners for parole, triage patients in emergency rooms, and predict where and when services might be needed. The next module will look at two different algorithmic case studies and explain how different definitions of fairness apply.</p>
-                    <div id={chartID} className="chart"></div>
-                    <h6 className="Small-Margin">Visualization shows different policy areas where algorithmically informed-decision making is currently in use. Case studies are highlighed in purple. Visualization data created from examples in <NavLink to="/Resources">O'Neil (2016) AINOW (2018), Eubanks (2018), and Obermeyer et al. (2019)</NavLink>.</h6>                </div>
             </div>
         </Overlay>:
         <></>
