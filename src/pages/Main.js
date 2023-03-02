95b872d84b1b4064eb0e6b2243cfa30d2639ae74
Joli Holmes <holmesjoli@gmail.com>
2023-03-02 10:20:36 -0500

    added subtitle

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -12,6 +12,9 @@ export default function Main() {
     return(
         <div className="Start">
             <h1>Risky Code</h1>
+            {/* <div className="Inner"> */}
+                <h3>facilitating deliberation about the impacts of algorithmically informed decision-making in public policy</h3>
+            {/* </div> */}
             {/* <Button variant="outlined" color="secondary" onClick={routeNext}>start</Button> */}
             <Button variant="contained" onClick={routeNext}>start</Button>
         </div>
