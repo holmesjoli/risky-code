ea59d42bb41ea04c5dbb7a6eed19cdfea3280b65
Joli Holmes <holmesjoli@gmail.com>
2023-03-09 14:53:08 -0500

    removed visualization

--- src/components/Visualization.js
+++
@@ -1,9 +0,0 @@
-// import Card from "./Card";
-
-export default function Visualization() {
-    return (
-        <div className="Visualization">
-            {/* <Card/> */}
-        </div>
-    )    
-}
