909c88f5525151610933b2658535f93c212c1966
Joli Holmes <holmesjoli@gmail.com>
2023-03-19 17:06:37 -0400

    fixed layout

--- src/pages/predict/Train.js
+++ src/pages/predict/Train.js
@@ -16,7 +16,7 @@ import MiniModel from "../../components/MiniStatisticalModel";
 
 function Information() {
     return (
-        <div className="Row Margin-Top">
+        <div className="Two-Row Margin-Top">
             <Actual/>
             <PredictiveOutcomes/>
         </div>
