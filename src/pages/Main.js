c439dac4348d21916d3bbd4a23256e352c53ce49
Joli Holmes <holmesjoli@gmail.com>
2023-03-25 14:45:17 -0400

    updated subtitle

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -12,7 +12,7 @@ export default function Main() {
     return(
         <div className="Start">
             <h1>Risky Code</h1>
-            <h3>facilitating deliberation about the impacts of algorithmically informed decision-making in public policy</h3>
+            <h3>a digital workshop for improving algorithmic literacy</h3>
             <Button variant="contained" onClick={routeNext}>start</Button>
         </div>
     )
