04d792069f7e245780bf2f871d0c9b12108b7c2f
Joli Holmes <holmesjoli@gmail.com>
2023-03-25 10:23:40 -0400

    fixed navlink

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 export default function Header() {
     return (
     <div className="Header">
-        <h1 className="No-Margin-Bottom"><NavLink to="/Classify">Risky Code</NavLink></h1>
+        <h1 className="No-Margin-Bottom"><NavLink to="/">Risky Code</NavLink></h1>
         <div className="Links">
             <NavLink className="Link" to="/Glossary">glossary</NavLink>
             <NavLink className="Link" to="/Resources">resources</NavLink>
